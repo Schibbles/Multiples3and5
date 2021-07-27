ce9bfa275137ea16025c96bc99af1c56bef3169b
David Schejbal <dwschejbal@outlook.com>
2021-07-27 14:32:55 -0500

    Final for Multiples

--- Multiples3and5/Program.cs
+++ Multiples3and5/Program.cs
@@ -28,7 +28,7 @@ namespace Multiples3and5
 
             int sum = 0;
 
-            for (int i = 0; i <= 1000; i++)
+            for (int i = 0; i < 1000; i++)
             {
                 if ((i%3 == 0) || (i%5 == 0))
                 {
